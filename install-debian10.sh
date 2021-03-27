8da8ff2c3b10702185f275c870a18c92ab48840b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-03-27 13:33:44 +0000

    add install-debian10.sh

---
+++ install-debian10.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Only support Debian 10
+
+if [ `whoami` != "root" ];then
+	echo "sudo or root is required!"
+    exit 1
+fi
+
+apt update
+
+apt install nginx -y
+apt install git -y
+
+curl https://ssl-config.mozilla.org/ffdhe2048.txt > /var/lib/nginx/dhparam.pem
+chmod +r /var/lib/nginx/dhparam.pem
+
+mkdir tmp
+cd tmp
+git clone https://github.com/FastGitORG/nginx-conf --depth=1
+
+cd nginx-conf
+
+cp *.conf /etc/ngins/sites-enabled
+
+mkdir -p /www/wwwroot/fg
+cp robots.txt /www/wwwroot/fg
+
+cd ..
+rm -fR nginx-conf
+cd ..
+rm -fR tmp
+
+# TODO: Put Certs
+echo "Put cert and keys."
+
+# nginx -t
+
+# systemctl enable nginx
+# systemctl start nginx
+# systemctl reload nginx
